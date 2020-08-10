e9739eb1f0faa8c0cc6100762f4a2a9fcfa7681a
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-10 12:26:25 -0600

    tempConversion function that passes tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,20 @@
-const ftoc = function() {
+const ftoc = function (fTemp) {
+	let cTemp = (fTemp - 32) * (5 / 9);
 
-}
+	if (cTemp !== 0) cTemp = parseFloat(cTemp.toFixed(1));
 
-const ctof = function() {
+	return cTemp;
+};
 
-}
+const ctof = function (cTemp) {
+	let fTemp = cTemp * (9 / 5) + 32;
+
+	if (fTemp !== 0) fTemp = parseFloat(fTemp.toFixed(1));
+
+	return fTemp;
+};
 
 module.exports = {
-  ftoc,
-  ctof
-}
+	ftoc,
+	ctof
+};
