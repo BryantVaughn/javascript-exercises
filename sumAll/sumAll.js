77a936e1c37d1d7dfce832773469208174200bf1
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-10 11:46:24 -0600

    sumAll function that passes tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+	if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number')
+		return 'ERROR';
 
-}
+	let sum = 0;
+	let min = Math.min(a, b);
+	let max = Math.max(a, b);
 
-module.exports = sumAll
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return sum;
+};
+
+module.exports = sumAll;
