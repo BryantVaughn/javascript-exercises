93f34172ee8cfad4acab67612fde60f662b480ee
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-14 21:12:49 -0600

    palindromes function with all tests passed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function() {
+const palindromes = function (str) {
+	// My original solution
+	// const letters = 'abcdefghijklmnopqrstuvwxyz';
+	// const strArr = str.toLowerCase().split('');
+	// const letterArr = strArr.filter((char) => {
+	// 	return letters.indexOf(char) >= 0;
+	// });
 
-}
+	// const len = letterArr.length;
+	// for (let i = 0; i < len / 2; i++) {
+	// 	if (letterArr[i] !== letterArr[len - i - 1]) return false;
+	// }
+	// return true;
 
-module.exports = palindromes
+	// pulled from solutions branch
+	let lowercaseStr = str.toLowerCase().replace(/[^A-Za-z]/g, '');
+	return lowercaseStr.split('').reverse().join('') === lowercaseStr;
+};
+
+module.exports = palindromes;
