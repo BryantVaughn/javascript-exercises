1011400c1e4dc29730844f3d345e82ff1a6d7983
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-14 21:58:21 -0600

    fibonacci function with tests passed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (fibPos) {
+	if (fibPos < 0) return 'OOPS';
+	if (fibPos === 0) return 0;
 
-}
+	let a = 0;
+	let b = 1;
+	for (let i = 1; i < fibPos; i++) {
+		let temp = b;
+		b = a + b;
+		a = temp;
+	}
+	return b;
+};
 
-module.exports = fibonacci
+module.exports = fibonacci;
